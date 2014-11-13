750b52d195b47b4bdec871f6814764d7541b5e50
Guillermo Polito <guillepol@gmail.com>
2014-11-13 19:32:10 -0200

    added some def implementations

--- src/main/scala/FinancialTool.scala
+++ src/main/scala/FinancialTool.scala
@@ -1,5 +1,8 @@
 import java.text.SimpleDateFormat
 import java.util.Date
+import java.io.File
+import scala.io.Source
+import scala.util.Try
 
 /** XXX
  * Created by f on 11/12/14.
@@ -14,19 +17,41 @@ object FinancialTool {
 
   val columnNames = Seq("Open","High","Low","Close","Volume", "AdjClose")
 
-  def findFiles(root: String, symbols: Set[Sym]): Seq[String] = ???
+  def findFiles(root: String, symbols: Set[Sym]): Seq[String] = symbols.toSeq match {
+    case Nil => Nil
+    case (sym +: xs) => {
+      val file = new File(root, sym + ".csv")
+      if (file.exists())
+        file.getAbsolutePath() +: findFiles(root, xs.toSet)
+      else
+        findFiles(root, xs.toSet)
+    }
+  }
 
-  def readLines(file: String):  Seq[String] = ???
+  def readLines(file: String):  Seq[String] = 
+    Source.fromFile(file).getLines.toSeq.tail.filter(!_.isEmpty)
 
-  def parseLine(line:String): Row  = ???
+  def parseLine(line: String): Row = {
+    val lineValues = line split ","
+    val zipList = columnNames zip lineValues.tail
+    val rowMap = zipList.foldLeft (Map[String, Double]()) ((map, t) => map + (t._1 -> stringToDouble(t._2))) 
+    Row(dateFromString(lineValues.head), rowMap)
+  }
 
-  def query(symbols: Seq[Sym], dates: Seq[Date], col: String): Map [Date, Seq[SymValue]] = ???
+  def query(root: String, symbols: Seq[Sym], dates: Seq[Date], col: String): Map [Date, Seq[SymValue]] = 
+    (symbols, dates) match {
+      case (syms, Nil) => Map()
+      case (Nil, dts) => Map()
+      case (s +: syms, d +: dts) => findFiles(root, )
+    }
 
 
   private def dateFromString(date: String): Date = {
-    val simpleDateFormat = new SimpleDateFormat("YYYY-MM-DD")
+    val simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd")
     simpleDateFormat.parse(date)
   }
 
-
+  private def stringToDouble(str: String): Double = {
+    Try(str.toDouble).getOrElse(0D)
+  }
 }
